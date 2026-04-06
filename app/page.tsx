20308e530ec489da263d810528ccc4d798f77b52
subway44 <ilya.yarosh@tripledotstudios.com>
2026-04-06 03:50:12 +0200

    feat: finalize hero section, add SEO structure and responsive fixes

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import Hero from "./sections/Hero";
+import SeoContent from "./sections/SeoContent";
 
 export default function Home() {
   return (
     <>
       <Hero />
+      <SeoContent />
     </>
   );
 }
